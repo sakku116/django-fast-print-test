5476c499829da7d46b2b3f012fc4d7348a549e79
zakky <kycaft1164@gmail.com>
2023-11-30 19:17:54 +0700

    feat: get category and status list APIs

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import products
+from .views import product, category, status
 
 urlpatterns = [
-    path("products", products.ProductApiView.as_view()),
-    path("products/<str:product_id>", products.ProductDetailApiView.as_view())
+    path("products", product.ProductApiView.as_view()),
+    path("products/<str:product_id>", product.ProductDetailApiView.as_view()),
+    path("statuses", status.StatusAPIView.as_view()),
+    path("categories", category.CategoryAPIView.as_view()),
 ]
---
+++ apps/api/views/status.py
@@ -0,0 +1,61 @@
+from django.core.paginator import Paginator
+from rest_framework.views import APIView
+
+from apps.api.models import Status
+from apps.api.serializers import StatusSerializer
+from utils.helper import parseBool
+from utils.resp import CustomResp
+
+
+class StatusAPIView(APIView):
+    def get(self, request):
+        try:
+            params = {
+                "page": int(request.query_params.get("page", "1")),
+                "limit": int(request.query_params.get("limit", "10")),
+                "show_all": request.query_params.get("show_all"),
+            }
+            if params["show_all"] not in ["None", None, "", "all"]:
+                params["show_all"] = parseBool(params["show_all"])
+        except Exception as e:
+            print(f"error: {e}")
+            return CustomResp(
+                error=True,
+                message="invalid query paramters",
+                error_detail=str(e),
+                status_code=400,
+            )
+
+        categories = Status.objects.all()
+        if not params["show_all"]:
+            paginator = Paginator(categories, params["limit"])
+            results = paginator.get_page(params["page"])
+            serializer = StatusSerializer(results, many=True)
+            return CustomResp(
+                data=serializer.data,
+                additional_field={
+                    "pagination_meta": {
+                        "total_page": paginator.num_pages,
+                        "total_data": paginator.count,
+                        "current_page": params["page"],
+                        "limit": params["limit"],
+                        "has_next": results.has_next(),
+                        "has_prev": results.has_previous(),
+                    }
+                },
+            )
+        else:
+            serializer = StatusSerializer(categories, many=True)
+            return CustomResp(
+                data=serializer.data,
+                additional_field={
+                    "pagination_meta": {
+                        "total_page": 0,
+                        "total_data": len(categories),
+                        "current_page": 0,
+                        "limit": 0,
+                        "has_next": False,
+                        "has_prev": False,
+                    }
+                },
+            )
